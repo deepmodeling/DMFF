19ecdbbf6d345343ab233206d4b7fd9d8c6447f5
WangXinyan940 <wangxy940930@gmail.com>
2022-03-28 00:08:31 +0800

    feat(classical): add testcases for NoCutoff Coulomb potential and a large GAFF molecule

--- tests/test_classical.py
+++ tests/test_classical.py
@@ -5,6 +5,7 @@ import openmm.unit as unit
 import numpy as np
 import numpy.testing as npt
 from dmff.api import Hamiltonian
+import dmff.api as api
 import pytest
 
 from dmff.classical.inter import LennardJonesForce
@@ -55,13 +56,9 @@ class TestClassical:
         energy = bondE(pos, box, pairs, h.getGenerators()[0].params)
         npt.assert_almost_equal(energy, value, decimal=3)
 
-
     @pytest.mark.parametrize(
         "pdb, prm, value",
-        [
-            ("data/lj2.pdb", "data/lj2.xml", -1.85001802444458)
-        ]
-    )
+        [("data/lj2.pdb", "data/lj2.xml", -1.85001802444458)])
     def test_lj_force(self, pdb, prm, value):
         pdb = app.PDBFile(pdb)
         h = Hamiltonian(prm)
@@ -71,17 +68,15 @@ class TestClassical:
                                    removeCMMotion=False)
         pos = pdb.getPositions(asNumpy=True).value_in_unit(unit.nanometer)
         box = np.array([[10.0, 0.0, 0.0], [0.0, 10.0, 0.0], [0.0, 0.0, 10.0]])
-        pairs = np.array([[0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3]], dtype=int)
+        pairs = np.array([[0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3]],
+                         dtype=int)
         ljE = h._potentials[0]
         energy = ljE(pos, box, pairs, h.getGenerators()[0].params)
         npt.assert_almost_equal(energy, value, decimal=3)
 
     @pytest.mark.parametrize(
         "pdb, prm, value",
-        [
-            ("data/lj3.pdb", "data/lj3.xml", -2.001220464706421)
-        ]
-    )
+        [("data/lj3.pdb", "data/lj3.xml", -2.001220464706421)])
     def test_lj_large_force(self, pdb, prm, value):
         pdb = app.PDBFile(pdb)
         h = Hamiltonian(prm)
@@ -93,9 +88,98 @@ class TestClassical:
         box = np.array([[10.0, 0.0, 0.0], [0.0, 10.0, 0.0], [0.0, 0.0, 10.0]])
         pairs = []
         for ii in range(10):
-            for jj in range(ii+1, 10):
+            for jj in range(ii + 1, 10):
                 pairs.append((ii, jj))
         pairs = np.array(pairs, dtype=int)
         ljE = h._potentials[0]
         energy = ljE(pos, box, pairs, h.getGenerators()[0].params)
         npt.assert_almost_equal(energy, value, decimal=3)
+
+    @pytest.mark.parametrize(
+        "pdb, prm, value",
+        [("data/lj2.pdb", "data/coul2.xml", 83.72177124023438)])
+    def test_coul_force(self, pdb, prm, value):
+        pdb = app.PDBFile(pdb)
+        h = Hamiltonian(prm)
+        system = h.createPotential(pdb.topology,
+                                   nonbondedMethod=app.NoCutoff,
+                                   constraints=None,
+                                   removeCMMotion=False)
+        pos = pdb.getPositions(asNumpy=True).value_in_unit(unit.nanometer)
+        box = np.array([[10.0, 0.0, 0.0], [0.0, 10.0, 0.0], [0.0, 0.0, 10.0]])
+        pairs = np.array([[0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3]],
+                         dtype=int)
+        coulE = h._potentials[0]
+        energy = coulE(pos, box, pairs, h.getGenerators()[0].params)
+        npt.assert_almost_equal(energy, value, decimal=3)
+
+    @pytest.mark.parametrize(
+        "pdb, prm, value",
+        [("data/lj3.pdb", "data/coul3.xml", -446.82037353515625)])
+    def test_coul_large_force(self, pdb, prm, value):
+        pdb = app.PDBFile(pdb)
+        h = Hamiltonian(prm)
+        system = h.createPotential(pdb.topology,
+                                   nonbondedMethod=app.NoCutoff,
+                                   constraints=None,
+                                   removeCMMotion=False)
+        pos = pdb.getPositions(asNumpy=True).value_in_unit(unit.nanometer)
+        box = np.array([[10.0, 0.0, 0.0], [0.0, 10.0, 0.0], [0.0, 0.0, 10.0]])
+        pairs = []
+        for ii in range(10):
+            for jj in range(ii + 1, 10):
+                pairs.append((ii, jj))
+        pairs = np.array(pairs, dtype=int)
+        coulE = h._potentials[0]
+        energy = coulE(pos, box, pairs, h.getGenerators()[0].params)
+        npt.assert_almost_equal(energy, value, decimal=3)
+
+    @pytest.mark.parametrize(
+        "pdb, prm, value",
+        [("data/lj3.pdb", "data/coul3-res.xml", -446.82037353515625)])
+    def test_coul_res_large_force(self, pdb, prm, value):
+        pdb = app.PDBFile(pdb)
+        h = Hamiltonian(prm)
+        system = h.createPotential(pdb.topology,
+                                   nonbondedMethod=app.NoCutoff,
+                                   constraints=None,
+                                   removeCMMotion=False)
+        pos = pdb.getPositions(asNumpy=True).value_in_unit(unit.nanometer)
+        box = np.array([[10.0, 0.0, 0.0], [0.0, 10.0, 0.0], [0.0, 0.0, 10.0]])
+        pairs = []
+        for ii in range(10):
+            for jj in range(ii + 1, 10):
+                pairs.append((ii, jj))
+        pairs = np.array(pairs, dtype=int)
+        coulE = h._potentials[0]
+        energy = coulE(pos, box, pairs, h.getGenerators()[0].params)
+        npt.assert_almost_equal(energy, value, decimal=3)
+
+    @pytest.mark.parametrize("pdb, prm, values", [
+        ("data/lig.pdb", ["data/gaff-2.11.xml", "data/lig-prm-lj.xml"], [
+            174.16702270507812, 99.81585693359375, 99.0631103515625,
+            22.778038024902344
+        ]),
+        #("data/lig.pdb", ["data/gaff-2.11.xml", "data/lig-prm.xml"], []),
+    ])
+    def test_gaff2_force(self, pdb, prm, values):
+        app.Topology.loadBondDefinitions("data/lig-top.xml")
+        pdb = app.PDBFile(pdb)
+        h = Hamiltonian(*prm)
+        system = h.createPotential(pdb.topology,
+                                   nonbondedMethod=app.NoCutoff,
+                                   constraints=None,
+                                   removeCMMotion=False)
+        pos = pdb.getPositions(asNumpy=True).value_in_unit(unit.nanometer)
+        box = np.array([[10.0, 0.0, 0.0], [0.0, 10.0, 0.0], [0.0, 0.0, 10.0]])
+        pairs = []
+        for ii in range(pos.shape[0]):
+            for jj in range(ii + 1, pos.shape[0]):
+                pairs.append((ii, jj))
+        pairs = np.array(pairs, dtype=int)
+
+        generators = h.getGenerators()
+        be_ref, ae_ref, tore_ref, lj_ref = values
+        for ne, energy in enumerate(h._potentials):
+            energy = energy(pos, box, pairs, h.getGenerators()[ne].params)
+            npt.assert_almost_equal(energy, values[ne], decimal=3)
