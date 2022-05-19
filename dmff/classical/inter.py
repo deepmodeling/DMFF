ab17cf2e78fcbe30f981ff60aad4d258dbc8eee4
Yingze Wang <ericwangyz@pku.edu.cn>
2022-05-19 21:17:54 +0800

    refactor(classical): add dispCorr

--- dmff/classical/inter.py
+++ dmff/classical/inter.py
@@ -1,13 +1,15 @@
-from dmff.admp.pairwise import distribute_scalar
-import jax.numpy as jnp
-from dmff.admp.pme import energy_pme, setup_ewald_parameters
-from dmff.admp.recip import generate_pme_recip
-from dmff.admp.spatial import v_pbc_shift
+from typing import Iterable
+
 import numpy as np
 import jax.numpy as jnp
-from jax import grad
+from jax.scipy.special import erfc, erf
+from dmff.utils import pair_buffer_scales
+from dmff.admp.pme import energy_pme, setup_ewald_parameters, pme_real
+from dmff.admp.recip import generate_pme_recip
+from dmff.admp.spatial import v_pbc_shift
 from dmff.admp.recip import generate_pme_recip, Ck_1
 from dmff.admp.pme import DIELECTRIC 
+
 
 ONE_4PI_EPS0 = DIELECTRIC * 0.1
 
@@ -20,9 +22,9 @@ class LennardJonesForce:
         map_prm,
         map_nbfix,
         colvmap,
-        isSwitch=False,
-        isPBC=True,
-        isNoCut=False,
+        isSwitch: bool = False,
+        isPBC: bool = True,
+        isNoCut: bool = False
     ) -> None:
         self.isSwitch = isSwitch
         self.r_switch = r_switch
@@ -40,9 +42,10 @@ class LennardJonesForce:
                 dr_vec = v_pbc_shift(dr_vec, box, jnp.linalg.inv(box))
             dr_norm = jnp.linalg.norm(dr_vec, axis=1)
             if not self.ifNoCut:
-                sig = sig[dr_norm <= self.r_cut]
-                eps = eps[dr_norm <= self.r_cut]
-                dr_norm = dr_norm[dr_norm <= self.r_cut]
+                msk = dr_norm <= self.r_cut
+                sig = sig[msk]
+                eps = eps[msk]
+                dr_norm = dr_norm[msk]
 
             dr_inv = 1.0 / dr_norm
             sig_dr = sig * dr_inv
@@ -51,11 +54,10 @@ class LennardJonesForce:
             E = 4.0 * eps * (sig_dr12 - sig_dr6)
 
             if self.isSwitch:
-
                 x = (dr_norm - self.r_switch) / (self.r_cut - self.r_switch)
                 S = 1 - 6. * x ** 5 + 15. * x ** 4 - 10. * x ** 3
                 jnp.where(dr_norm > self.r_switch, E, E * S)
-
+            
             return E
 
         def get_energy(positions, box, pairs, epsilon, sigma, epsfix, sigfix, mscales):
@@ -74,7 +76,6 @@ class LennardJonesForce:
 
             colv_pair = self.colvmap[pairs[:,0],pairs[:,1]]
             mscale_pair = mscales[colv_pair-1] # in mscale vector, the 0th item is 1-2 scale, the 1st item is 1-3 scale, etc...
-            
 
             dr_vec = positions[pairs[:, 0]] - positions[pairs[:, 1]]
             prm_pair0 = self.map_prm[pairs[:, 0]]
@@ -90,6 +91,45 @@ class LennardJonesForce:
 
         return get_energy
 
+
+class LennardJonesLongRangeForce:
+    def __init__(
+        self,
+        r_cut: float,
+        map_prm: Iterable[int],
+        map_nbfix: Iterable[int],
+        countMat: np.ndarray
+    ):
+        self.r_cut = r_cut
+        self.map_prm = map_prm
+        self.map_nbfix = map_nbfix
+        self.countMat = countMat
+        self.numParticles = len(map_prm)
+    
+    def generate_get_energy(self):
+        def get_energy(box, epsilon, sigma, epsfix, sigfix):
+
+            eps_m1 = jnp.repeat(epsilon.reshape((-1, 1)), epsilon.shape[0], axis=1)
+            eps_m2 = eps_m1.T
+            eps_mat = jnp.sqrt(eps_m1 * eps_m2)
+            sig_m1 = jnp.repeat(sigma.reshape((-1, 1)), sigma.shape[0], axis=1)
+            sig_m2 = sig_m1.T
+            sig_mat = (sig_m1 + sig_m2) * 0.5
+
+            eps_mat = eps_mat.at[self.map_nbfix[:, 0], self.map_nbfix[:, 1]].set(epsfix)
+            eps_mat = eps_mat.at[self.map_nbfix[:, 1], self.map_nbfix[:, 0]].set(epsfix)
+            sig_mat = sig_mat.at[self.map_nbfix[:, 0], self.map_nbfix[:, 1]].set(sigfix)
+            sig_mat = sig_mat.at[self.map_nbfix[:, 1], self.map_nbfix[:, 0]].set(sigfix)
+
+            volume = jnp.linalg.det(box)
+
+            c6Mat = 4 * eps_mat * jnp.power(sig_mat, 6)
+            c6 = jnp.sum(c6Mat * self.countMat) / jnp.sum(self.countMat)
+            dispCorrEnergy = -2 / 3 * jnp.pi * self.numParticles * self.numParticles / volume * c6 / jnp.power(self.r_cut, 3)
+            return dispCorrEnergy
+        
+        return get_energy
+    
 
 class CoulNoCutoffForce:
     # E=\frac{{q}_{1}{q}_{2}}{4\pi\epsilon_0\epsilon_1 r}
@@ -189,113 +229,127 @@ class CoulReactionFieldForce:
 
 
 class CoulombPMEForce:
-    def __init__(self, box, rc, ethresh, covalent_map):
-
-        self.kappa, self.K1, self.K2, self.K3 = setup_ewald_parameters(rc, ethresh, box)
-
-        self.covalent_map = covalent_map
-        self.refresh_calculator()
+    def __init__(
+        self,
+        r_cut: float,
+        map_prm: Iterable[int],
+        colv_map: np.ndarray,
+        ethresh: float,
+        pme_order: int = 6
+    ):
+        self.r_cut = r_cut
+        self.map_prm = map_prm
+        self.ethresh = ethresh
+        self.pme_order = pme_order
+        self.colv_map = colv_map
+        self.lmax = 0
 
     def generate_get_energy(self):
-        def get_energy(positions, box, pairs, Q, mScales):
+        
+        def get_energy(positions, box, pairs, charges, mscales):
+            # kappa, K1, K2, K3 = setup_ewald_parameters(self.r_cut, self.ethresh, box)
+            # print(kappa, K1, K2, K3)
+            kappa = 3.458910000000000 / 10
+            K1 = 33
+            K2 = 33
+            K3 = 33
+            self.r_cut = 1.0
+
+            atomCharges = charges[self.map_prm[np.arange(positions.shape[0])]]
+            atomChargesT = atomCharges.reshape(-1, 1)
+            # # print(mscales)
+
+            bufscales = pair_buffer_scales(pairs)
+
+            # mscales = jnp.array([0.0, 0.0, 0.0, 1.0, 1.0, 1.0], dtype=jnp.float32) # debug
+            colv_pair = self.colv_map[pairs[:, 0], pairs[:, 1]]
+            mscale_pair = mscales[colv_pair-1]
+
+
+            
+            dr_vec = positions[pairs[:, 0]] - positions[pairs[:, 1]]
+            dr_vec = v_pbc_shift(dr_vec, box, jnp.linalg.inv(box))
+            
+            dr_norm = jnp.linalg.norm(dr_vec, axis=1)
+
+            
+            # msk = dr_norm <= self.r_cut
+            
+            # dr_norm = dr_norm[msk]
+            # erfc_dr = erfc(dr_norm * kappa)
+
+            chgprod = atomCharges[pairs[:, 0]] * atomCharges[pairs[:, 1]]
+            erf_dr = erf(dr_norm * kappa * 10)
+            ene = chgprod * (mscale_pair - erf_dr) / dr_norm * bufscales * ONE_4PI_EPS0
+            print(jnp.sum(ene))
+
+            # chgprod = chgprod[msk]
+            # mscale_pair = mscale_pair[msk]
+            # ene_vec = chgprod * erfc_dr / dr_norm * ONE_4PI_EPS0 * mscale_pair
+            # short_range_ene = jnp.sum(ene_vec)
+            # print("Short Range:", short_range_ene)
+            # self_corr_ene = -ONE_4PI_EPS0 * kappa / jnp.sqrt(jnp.pi) * jnp.sum(atomCharges ** 2)
+            # # 1-2, 1-3 excl
+            # excl_msk = jnp.logical_and(
+            #     jnp.logical_or(
+            #         jnp.logical_or(colv_pair == 1, colv_pair == 2),
+            #         colv_pair == 3
+            #     ),
+            #     bufscales
+            # )
+            # excl_ene_vec = -chgprod * (1 - erfc_dr) / dr_norm * ONE_4PI_EPS0
+            # excl_ene = jnp.sum(excl_ene_vec[excl_msk])
+
+            # self_corr_ene += excl_ene
+            # print("Self Corr:", self_corr_ene)
+            # # print(jnp.mean(erfc_dr))
+
+            # # # dr_shift = erfc(kappa * self.r_cut) / self.r_cut
+            # # # ewald_shift = jnp.sum(chgprod * dr_shift * ONE_4PI_EPS0 * mscale_pair)
+            # # # print("Shift:", ewald_shift)
+            # # # short_range_ene -= ewald_shift
+            # dipole = jnp.sum(atomChargesT * positions, axis=0)
+            # # # ene_dipole = jnp.dot(dipole, dipole.T) * ONE_4PI_EPS0
+            # # # print(ene_dipole)
+            # print(-jnp.sum(dipole ** 2) * ONE_4PI_EPS0 * 3 * jnp.pi / 2 / jnp.linalg.det(box))
+            # # # print(atomCharges)
+
+            pme_recip_fn = generate_pme_recip(
+                Ck_fn=Ck_1,
+                kappa=kappa,
+                gamma=False,
+                pme_order=self.pme_order,
+                K1=K1,
+                K2=K2,
+                K3=K3,
+                lmax=self.lmax,
+            )
+            # ene_recip = pme_recip_fn(positions, box, atomChargesT) * 0.1
+            # print("Coul. recip:", ene_recip)
+
+            # return short_range_ene + self_corr_ene + ene_recip
+
 
             return energy_pme(
-                positions,
-                box,
+                positions * 10,
+                box * 10,
                 pairs,
-                Q,
+                atomChargesT,
                 None,
                 None,
                 None,
-                mScales,
+                mscales,
                 None,
                 None,
-                self.covalent_map,
+                self.colv_map,
                 None,
-                self.pme_recip,
-                self.kappa,
-                self.K1,
-                self.K2,
-                self.K3,
+                pme_recip_fn,
+                kappa,
+                K1,
+                K2,
+                K3,
                 self.lmax,
                 False,
             )
 
         return get_energy
-
-    def refresh_calculator(self):
-
-        self.construct_local_frames = None
-        lmax = 0
-        self.pme_recip = generate_pme_recip(
-            Ck_1, self.kappa, False, self.pme_order, self.K1, self.K2, self.K3, lmax
-        )
-
-        self.get_energy = self.genreate_get_energy()
-
-        return
-
-
-if __name__ == "__main__":
-
-    # atoms: 0, 1, 2, 3
-    # exclusion: 0 - 1, 2 - 3
-    # nbfix: 0 - 3 (3., 0.3)
-    positions = jnp.array([[0, 0, 0], [1, 0, 0], [0, 1, 0], [1, 0, 1]], dtype=float)
-    p_ref = np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0], [1, 0, 1]], dtype=float)
-
-    box = jnp.array([[10, 0, 0], [0, 10, 0], [0, 0, 10]])
-
-    pairs = np.array([[0, 1], [0, 2], [1, 2], [1, 3], [2, 3]])
-    covalent_map = np.array(
-        [[0, 1, 1, 0], [1, 0, 1, 1], [1, 1, 0, 1], [0, 1, 1, 0]]
-    )  # NOTE:not sure
-    pairs_ref = np.array([[0, 2], [0, 3], [1, 2], [1, 3]])
-
-    epsilon = jnp.array([1.0, 2.0])
-    sigma = jnp.array([0.5, 0.6])
-
-    map_prm = np.array([0, 0, 1, 1])
-    map_nbfix = np.array([[0, 3]])
-    epsfix = jnp.array([3.0])
-    sigfix = jnp.array([0.8])
-    map_exclusion = np.array([[0, 1], [2, 3]])
-    scale_exclusion = jnp.array([1.0, 1.0])
-    map_14 = np.array([[]])
-
-    lj = LennardJonesForce(0, 3, map_prm, map_nbfix, map_exclusion, scale_exclusion)
-    get_energy = lj.generate_get_energy()
-
-    E = get_energy(positions, box, pairs, epsilon, sigma, epsfix, sigfix)
-
-    # Eref
-    eps0 = epsilon[map_prm[pairs_ref[:, 0]]]
-    eps1 = epsilon[map_prm[pairs_ref[:, 1]]]
-    sig0 = sigma[map_prm[pairs_ref[:, 0]]]
-    sig1 = sigma[map_prm[pairs_ref[:, 1]]]
-
-    eps = np.sqrt(eps0 * eps1)
-    sig = (sig0 + sig1) / 2.0
-    p0 = p_ref[pairs_ref[:, 0]]
-    p1 = p_ref[pairs_ref[:, 1]]
-    dr_vec = p1 - p0
-    dr = np.sqrt(np.power(dr_vec, 2).sum(axis=1))
-    sig_dr = sig / dr
-    Eref = 4.0 * eps * (np.power(sig_dr, 12) - np.power(sig_dr, 6))
-    Eref = Eref.sum()
-    dr_fix = p_ref[0] - p_ref[3]
-    dr_fix = np.sqrt((dr_fix * dr_fix).sum())
-    Eref += 4.0 * 3.0 * ((0.3 / dr_fix) ** 12 - (0.3 / dr_fix) ** 6)
-
-    print(E, "vs", Eref)
-    F = grad(get_energy)(positions, box, pairs, epsilon, sigma, epsfix, sigfix)
-    print(F)
-
-    # test PME
-    rc = 4
-    ethresh = 1e-4
-    mScales = np.array([1.0, 1.0, 1.0])
-    Q = np.array([0.1, 0.1])
-    pme = CoulombPMEForce(box, rc, ethresh)
-    get_energy = pme.generate_get_energy()
-    E = get_energy(positions, box, pairs, Q, mScales)
