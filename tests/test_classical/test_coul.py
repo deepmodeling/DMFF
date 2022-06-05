27d9576495fd6f662296b4c170ecc3923e178ea7
Yingze Wang <ericwangyz@pku.edu.cn>
2022-06-06 00:47:19 +0800

    refactor(test_classical): split to several files

--- tests/test_classical/test_coul.py
+++ tests/test_classical/test_coul.py
@@ -1,3 +1,4 @@
+import pytest
 import jax
 import jax.numpy as jnp
 import openmm.app as app
@@ -5,16 +6,14 @@ import openmm.unit as unit
 import numpy as np
 import numpy.testing as npt
 from dmff.api import Hamiltonian
-import pytest
-from dmff import NeighborList
 
 
-class TestVdW:
-    
+class TestCoulomb:
+
     @pytest.mark.parametrize(
         "pdb, prm, value",
-        [("tests/data/lj2.pdb", "tests/data/lj2.xml", -1.85001802444458)])
-    def test_lj_force(self, pdb, prm, value):
+        [("tests/data/lj2.pdb", "tests/data/coul2.xml", 83.72177124023438)])
+    def test_coul_force(self, pdb, prm, value):
         pdb = app.PDBFile(pdb)
         h = Hamiltonian(prm)
         system = h.createPotential(pdb.topology,
@@ -22,22 +21,20 @@ class TestVdW:
                                    constraints=None,
                                    removeCMMotion=False)
         pos = jnp.asarray(pdb.getPositions(asNumpy=True).value_in_unit(unit.nanometer))
-        box = jnp.array([[10.0, 0.0, 0.0], [0.0, 10.0, 0.0], [0.0, 0.0, 10.0]])
-        nblist = NeighborList(box, 4.0)
-        nblist.allocate(pos)
-        pairs = nblist.pairs
-        ljE = h._potentials[0]
-        energy = ljE(pos, box, pairs, h.getGenerators()[0].params)
+        box = np.array([[10.0, 0.0, 0.0], [0.0, 10.0, 0.0], [0.0, 0.0, 10.0]])
+        pairs = np.array([[0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3]],
+                         dtype=int)
+        coulE = h._potentials[0]
+        energy = coulE(pos, box, pairs, h.getGenerators()[0].params)
         npt.assert_almost_equal(energy, value, decimal=3)
         
-        energy = jax.jit(ljE)(pos, box, pairs, h.getGenerators()[0].params)
+        energy = jax.jit(coulE)(pos, box, pairs, h.getGenerators()[0].params)
         npt.assert_almost_equal(energy, value, decimal=3)
-        
 
     @pytest.mark.parametrize(
         "pdb, prm, value",
-        [("tests/data/lj3.pdb", "tests/data/lj3.xml", -2.001220464706421)])
-    def test_lj_large_force(self, pdb, prm, value):
+        [("tests/data/lj3.pdb", "tests/data/coul3.xml", -446.82037353515625)])
+    def test_coul_large_force(self, pdb, prm, value):
         pdb = app.PDBFile(pdb)
         h = Hamiltonian(prm)
         system = h.createPotential(pdb.topology,
@@ -51,30 +48,33 @@ class TestVdW:
             for jj in range(ii + 1, 10):
                 pairs.append((ii, jj))
         pairs = np.array(pairs, dtype=int)
-        ljE = h._potentials[0]
-        energy = ljE(pos, box, pairs, h.getGenerators()[0].params)
+        coulE = h._potentials[0]
+        energy = coulE(pos, box, pairs, h.getGenerators()[0].params)
         npt.assert_almost_equal(energy, value, decimal=3)
         
-        energy = jax.jit(ljE)(pos, box, pairs, h.getGenerators()[0].params)
+        energy = jax.jit(coulE)(pos, box, pairs, h.getGenerators()[0].params)
         npt.assert_almost_equal(energy, value, decimal=3)
-        
-    def test_lj_params_check(self):
-        pdb = app.PDBFile("tests/data/lj3.pdb")
-        h = Hamiltonian("tests/data/lj3.xml")
+
+    @pytest.mark.parametrize(
+        "pdb, prm, value",
+        [("tests/data/lj3.pdb", "tests/data/coul3-res.xml", -446.82037353515625)])
+    def test_coul_res_large_force(self, pdb, prm, value):
+        pdb = app.PDBFile(pdb)
+        h = Hamiltonian(prm)
         system = h.createPotential(pdb.topology,
                                    nonbondedMethod=app.NoCutoff,
                                    constraints=None,
                                    removeCMMotion=False)
-        pos = pdb.getPositions(asNumpy=True).value_in_unit(unit.nanometer)
+        pos = jnp.asarray(pdb.getPositions(asNumpy=True).value_in_unit(unit.nanometer))
         box = np.array([[10.0, 0.0, 0.0], [0.0, 10.0, 0.0], [0.0, 0.0, 10.0]])
         pairs = []
         for ii in range(10):
             for jj in range(ii + 1, 10):
                 pairs.append((ii, jj))
-        pairs = np.array(pairs, dtype=int)                                        
-        ljE = h._potentials[0]
-        with pytest.raises(TypeError):
-            energy = ljE(pos, box, pairs, h.getGenerators()[0].params)
-            
-        energy = jax.jit(ljE)(pos, box, pairs, h.getGenerators()[0].params)  # jit will optimized away type check
-        force = jax.grad(jax.jit(ljE))(pos, box, pairs, h.getGenerators()[0].params)
+        pairs = np.array(pairs, dtype=int)
+        coulE = h._potentials[0]
+        energy = coulE(pos, box, pairs, h.getGenerators()[0].params)
+        npt.assert_almost_equal(energy, value, decimal=3)
+        
+        energy = jax.jit(coulE)(pos, box, pairs, h.getGenerators()[0].params)
+        npt.assert_almost_equal(energy, value, decimal=3)
