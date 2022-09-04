290073cf6aaae054b0767fdb658502ab64774097
Yingze Wang <ericwangyz@pku.edu.cn>
2022-09-04 00:33:40 -0700

    update(setup): setuptools_scm in requirements

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ with open(path.join('dmff', '_date.py'), 'w') as fp:
     fp.write('date = \'%s\'' % today)
 
 install_requires = [
+    "setuptools_scm",
     "numpy>=1.18",
     "jax_md>=0.1.28",
     "openmm",
