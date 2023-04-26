71c3fa624070fd25904c60fac1f6e3b4b46c5865
WangXinyan940 <44082181+WangXinyan940@users.noreply.github.com>
2023-04-27 02:12:56 +0800

    Initialize generators before loading hamiltonian

--- dmff/__init__.py
+++ dmff/__init__.py
@@ -1,5 +1,5 @@
 from .settings import *
 from .common.nblist import NeighborListFreud
-from .api.hamiltonian import Hamiltonian
 from .generators import *
+from .api.hamiltonian import Hamiltonian
 from .operators import *
