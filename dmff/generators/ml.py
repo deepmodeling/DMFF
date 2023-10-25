8963db330d95f7d15525bc99681009afe85ab84b
Jeremydream <dreamchen62@gmail.com>
2023-10-25 22:37:25 +0800

    Add frontend for EANN

--- dmff/generators/ml.py
+++ dmff/generators/ml.py
@@ -12,7 +12,7 @@ import pickle
 
 from ..sgnn.graph import MAX_VALENCE, TopGraph, from_pdb
 from ..sgnn.gnn import MolGNNForce, prm_transform_f2i
-
+from ..eann.eann import EANNForce, get_elem_indices
 
 class SGNNGenerator:
     def __init__(self, ffinfo: dict, paramset: ParamSet):
@@ -56,20 +56,12 @@ class SGNNGenerator:
         n_atoms = topdata.getNumAtoms()
         self.model = MolGNNForce(self.G, nn=self.nn)
         n_layers = self.model.n_layers
-
-        has_aux = False
-        if "has_aux" in kwargs and kwargs["has_aux"]:
-            has_aux = True
-
-        def potential_fn(positions, box, pairs, params, aux=None):
+        def potential_fn(positions, box, pairs, params):
             # convert unit to angstrom
             positions = positions * 10
             box = box * 10
             prms = prm_transform_f2i(params[self.name], n_layers)
-            if has_aux:
-                return self.model.get_energy(positions, box, prms), aux
-            else:
-                return self.model.get_energy(positions, box, prms)
+            return self.model.get_energy(positions, box, prms)
 
         self._jaxPotential = potential_fn
         return potential_fn
@@ -77,6 +69,66 @@ class SGNNGenerator:
     def getJaxPotential(self):
         return self._jaxPotential
 
-
 _DMFFGenerators["SGNNForce"] = SGNNGenerator
+
+class EANNGenerator:
+    def __init__(self, ffinfo: dict, paramset: ParamSet):
+
+        self.name = "EANNForce"
+        self.ffinfo = ffinfo
+        paramset.addField(self.name)
+        self.key_type = None
+
+        self.file = self.ffinfo["Forces"][self.name]["meta"]["file"]
+        self.ngto = int(self.ffinfo["Forces"][self.name]["meta"]["ngto"])
+        self.nipsin = int(self.ffinfo["Forces"][self.name]["meta"]["nipsin"])
+        self.rc = int(self.ffinfo["Forces"][self.name]["meta"]["rc"])
+
+        self.pdb = self.ffinfo["Forces"][self.name]["meta"]["pdb"]
+        self.ommtopology = app.PDBFile(self.pdb).topology
+        # load ML potential parameters
+        with open(self.file, 'rb') as ifile:
+            params = pickle.load(ifile)
+        self.params = params
+        # convert to jnp array
+        for k in params:
+            params[k] = params[k]
+            # set mask to all true
+            paramset.addParameter(params[k], k, field=self.name, mask=jnp.ones(params[k].shape))
+
+        # mask = jax.tree_util.tree_map(lambda x: jnp.ones(x.shape), params)
+        # paramset.addParameter(params, "params", field=self.name, mask=mask)
+       
+
+    def getName(self) -> str:
+        return self.name
+
+    def overwrite(self, params):
+        # do not use xml to handle ML potentials
+        # for ML potentials, xml only documents param file path
+        # so for ML potentials, overwrite function overwrites the file directly
+        with open(self.file, 'wb') as ofile:
+            pickle.dump(paramset[self.name], ofile)
+        return
+
+    def createPotential(self, topdata: DMFFTopology, nonbondedMethod, nonbondedCutoff, **kwargs):
+        n_atoms = topdata.getNumAtoms()
+        n_elem, elem_indices = get_elem_indices(self.ommtopology)
+        self.model = EANNForce(n_elem, elem_indices, n_gto=self.ngto, nipsin=self.nipsin, rc=self.rc)
+        n_layers = self.model.n_layers
+        def potential_fn(positions, box, pairs, params):
+            # convert unit to angstrom
+            positions = positions * 10
+            box = box * 10
+            
+            return self.model.get_energy(positions, box, pairs, params[self.name])
+
+        self._jaxPotential = potential_fn
+        return potential_fn
+
+    def getJaxPotential(self):
+        return self._jaxPotential
+
+_DMFFGenerators["EANNForce"] = EANNGenerator
+
 
