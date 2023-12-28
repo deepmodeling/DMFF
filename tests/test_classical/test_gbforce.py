ce84c9cf9fba80864643921f19469c1ad0667026
Ethan-Norch <56433364+Ethan-Norch@users.noreply.github.com>
2023-12-28 15:40:18 +0800

    Add files via upload

---
+++ tests/test_classical/test_gbforce.py
@@ -0,0 +1,33 @@
+import pytest
+import jax
+import jax.numpy as jnp
+import openmm.app as app
+import openmm.unit as unit
+import numpy as np
+import numpy.testing as npt
+from dmff.api import Hamiltonian
+from dmff.common import nblist
+
+
+@pytest.mark.parametrize(
+    "pdb, prm, value",
+    [
+        ("../data/10p.pdb", "../data/1_5corrV2.xml", -11184.921239189738),
+        ("../data/pBox.pdb", "../data/polyp_amberImp.xml", -13914.34177591779),
+    ])
+def test_custom_gb_force(pdb, prm, value):
+    pdb = app.PDBFile(pdb)
+    h = Hamiltonian(prm)
+    potential = h.createPotential(
+        pdb.topology,
+        nonbondedMethod=app.NoCutoff
+    )
+    pos = jnp.asarray(pdb.getPositions(asNumpy=True).value_in_unit(unit.nanometer))
+    box = np.array([[20.0, 0.0, 0.0], [0.0, 20.0, 0.0], [0.0, 0.0, 20.0]])
+    rc = 6.0
+    nbl = nblist.NeighborList(box, rc, potential.meta['cov_map'])
+    nbl.allocate(pos)
+    pairs = nbl.pairs
+    gbE = potential.getPotentialFunc(names=["CustomGBForce"])
+    energy = gbE(pos, box, pairs, h.paramset)
+    npt.assert_almost_equal(energy, value, decimal=3)
