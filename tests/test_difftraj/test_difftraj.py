8a228ed3943623fbf21ac826be11885db777fa71
ice-hanbin <ice8618833@gmail.com>
2024-03-21 10:50:36 +0800

    updata test_difftraj file

--- tests/test_difftraj/test_difftraj.py
+++ tests/test_difftraj/test_difftraj.py
@@ -24,7 +24,7 @@ mass = jnp.tile(jnp.array([m_O, m_H, m_H]), 216)
 
 @pytest.mark.parametrize(
     "pdbfile, prm, values",
-    [("tests/data/water_nvt.pdb", "tests/data/qspc-fw.xml", 5412.632150233606)])
+    [("tests/data/water_nvt.pdb", "tests/data/qspc-fw.xml", 5412.3623)])
 def test_difftraj(pdbfile, prm, values):
 
     pdb = PDBFile(pdbfile)
